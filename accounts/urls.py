ac79817eb33506f57d30f97ed9a75cf30a2a2306
vikrampatel0408 <96529637+vikrampatel0408@users.noreply.github.com>
2023-03-29 23:38:34 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('employee/profile/update', EditProfileView.as_view(), name='employer-profile-update'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('login', LoginView.as_view(), name='login'),
+     path('About.html',Aboutview),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
